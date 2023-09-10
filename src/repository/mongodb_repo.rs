ec35045b9a3c86c2766e616daa881ae225b5fbee
Gimahni Harshika <89870885+GimhaniHM@users.noreply.github.com>
2023-09-11 04:33:57 +0530

    add create_user()

--- src/repository/mongodb_repo.rs
+++ src/repository/mongodb_repo.rs
@@ -16,17 +16,37 @@ pub struct MongoRepo {
 }
 
 impl MongoRepo {
+    //
     pub async fn init() -> Self {
         dotenv().ok();
+        //load environment variable
         let uri = match env::var(MONGOURI) {
             Ok(v) => v.to_string(),
-            Err(_) => format!(Error loading env variable),
+            Err(_) => format!("Error loading env variable"),
         };
 
         let client = Client::with_uri_str(uri).unwrap();
-        let db = client.database("rustDB");
-        let col:Collection<User> = db.Collection("User");
+        let db = client.database("rustDB");     //create connection to the database
+        let col:Collection<User> = db.Collection("User");       //
         MongoRepo { col }
+    }
+
+    pub async fn create_user(&self, new_user: User) -> Result<InsertOneResult, Error> {
+        let new_doc = User {
+            id: None,
+            name: new_user.name,
+            location: new_user.location,
+            title: new_user.title,
+        };
+
+        let user = self
+            .col
+            .insert_one(new_doc, None)
+            .ok()
+            .await
+            .expect("Error creating user");
+
+        Ok(user)
     }
 
 }
